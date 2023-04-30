49caf168a9d3583ed7cc001daf752d76bf8c8704
Di Lu <dluisnothere@gmail.com>
2023-04-30 17:11:55 -0400

    Push bogus HH scaff case

--- foldMain.py
+++ foldMain.py
@@ -3099,3 +3099,175 @@ def test_fold_t_h_scaff():
             print(sol.projected_region)
 
 # test_fold_t_h_scaff()
+
+
+def test_fold_tt_scaff():
+
+    coords1 = np.array([(0, 1, 0), (1, 1, 0), (1, 1, 1), (0, 1, 1)])  # base 1
+
+    coords2 = np.array([(0.25, 0, 1), (0.25, 0, 0), (0.25, 1, 0), (0.25, 1, 1)])  # fold1
+    coords3 = np.array([(0.75, 0, 1), (0.75, 0, 0), (0.75, 1, 0), (0.75, 1, 1)])  # fold2
+
+    b1 = Patch(coords1)
+
+    f1 = Patch(coords2)
+    f2 = Patch(coords3)
+
+    b1.id = 0
+    # b2.id = 1
+
+    f1.id = 2
+    f2.id = 3
+
+    nodes = [b1, f1, f2]
+
+    edges = [[0, 1], [0, 2]]
+
+    push_dir = YAxis
+
+    input = InputScaff(nodes, edges, push_dir, 1, 1, 2, 0.5)
+
+    input.gen_hinge_graph()
+
+    for l in range(0, len(input.node_list)):
+        print(input.node_list[l].id)
+        print(input.node_list[l].patch_type)
+        print(list(input.hinge_graph.neighbors(l)))
+        print("------------")
+
+    print(input.hinge_graph)
+
+    input.gen_basic_scaffs()
+
+    print(input.basic_scaffs)
+
+    print("MIDSCAFFS")
+
+    input.gen_mid_scaffs()
+
+    print(input.mid_scaffs)
+
+    input.gen_fold_options()
+
+    input.order_folds()
+
+    # Generate solutions
+    # input.fold()
+
+    for mid_scaff in input.mid_scaffs_ordered:
+        print("MID SCAFF ID")
+        for basic_scaff in mid_scaff.basic_scaffs:
+            # print("FOLD PATCH COORDS")
+            # print(basic_scaff.f_patch.coords)
+            # offset = indexTime * 90
+
+            print("FOLD SOLUTION FOR: " + str(basic_scaff.id) + "===================")
+            sol: FoldOption = basic_scaff.optimal_fold_option
+            print("start time:")
+            print(basic_scaff.offset + basic_scaff.start_time)
+            print("end time:")
+            print(basic_scaff.offset + basic_scaff.end_time)
+            print("num hinges: ")
+            print(sol.modification.num_hinges)
+            print("num shrinks: ")
+            print(sol.modification.num_pieces)
+            print("range start: ")
+            print(sol.modification.range_start)
+            print("range end: ")
+            print(sol.modification.range_end)
+            print("isleft:")
+            print(sol.isleft)
+            print("original vertices: ")
+            print(basic_scaff.f_patch.coords)
+            print("Projected region of solution: ")
+            print(sol.projected_region)
+
+# test_fold_tt_scaff()
+
+def test_fold_hh_scaff():
+    coords1 = np.array([(0, 1, 0), (2, 1, 0), (2, 1, 1), (0, 1, 1)])  # base 1
+
+    coords2 = np.array([(0.5, 0, 1), (0.5, 0, 0), (0.5, 1, 0), (0.5, 1, 1)])  # fold1
+    coords3 = np.array([(1.5, 0, 1), (1.5, 0, 0), (1.5, 1, 0), (1.5, 1, 1)])  # fold2
+
+    coords4 = np.array([(0, 0, 0), (0.8, 0, 0), (0.8, 0, 1), (0, 0, 1)])  # base 2
+    coords5 = np.array([(1.2, 0, 0), (2, 0, 0), (2, 0, 1), (1.2, 0, 1)])  # fold3
+
+    b1 = Patch(coords1)
+    b2 = Patch(coords4)
+    b3 = Patch(coords5)
+
+    f1 = Patch(coords2)
+    f2 = Patch(coords3)
+
+    b1.id = 0
+    b2.id = 1
+    b3.id = 2
+
+    f1.id = 3
+    f2.id = 4
+
+    nodes = [b1, b2, b3, f1, f2]
+
+    edges = [[1, 3], [3, 0], [2, 4], [4, 0]]
+
+    push_dir = YAxis
+
+    input = InputScaff(nodes, edges, push_dir, 1, 1, 2, 0.5)
+
+    input.gen_hinge_graph()
+
+    for l in range(0, len(input.node_list)):
+        print(input.node_list[l].id)
+        print(input.node_list[l].patch_type)
+        print(list(input.hinge_graph.neighbors(l)))
+        print("------------")
+
+    print(input.hinge_graph)
+
+    input.gen_basic_scaffs()
+
+    print(input.basic_scaffs)
+
+    print("MIDSCAFFS")
+
+    input.gen_mid_scaffs()
+
+    print(input.mid_scaffs)
+
+    input.gen_fold_options()
+
+    input.order_folds()
+
+    # Generate solutions
+    # input.fold()
+
+    for mid_scaff in input.mid_scaffs_ordered:
+        print("MID SCAFF ID")
+        for basic_scaff in mid_scaff.basic_scaffs:
+            # print("FOLD PATCH COORDS")
+            # print(basic_scaff.f_patch.coords)
+            # offset = indexTime * 90
+
+            print("FOLD SOLUTION FOR: " + str(basic_scaff.id) + "===================")
+            sol: FoldOption = basic_scaff.optimal_fold_option
+            print("start time:")
+            print(basic_scaff.offset + basic_scaff.start_time)
+            print("end time:")
+            print(basic_scaff.offset + basic_scaff.end_time)
+            print("num hinges: ")
+            print(sol.modification.num_hinges)
+            print("num shrinks: ")
+            print(sol.modification.num_pieces)
+            print("range start: ")
+            print(sol.modification.range_start)
+            print("range end: ")
+            print(sol.modification.range_end)
+            print("isleft:")
+            print(sol.isleft)
+            print("original vertices: ")
+            print(basic_scaff.f_patch.coords)
+            print("Projected region of solution: ")
+            print(sol.projected_region)
+
+test_fold_hh_scaff()
